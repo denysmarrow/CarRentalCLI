8f82e2d391bd765da9112d25c5b21b0c358db7fe
denysmarrow <mzmarrowz@gmail.com>
2022-01-24 01:02:58 -0500

    adding car class

---
+++ src/com/careerdevs/RentalService.java
@@ -0,0 +1,15 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class RentalService {
+
+    Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Car Rental CLI");
+
+
+    }
+}
