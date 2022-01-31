b312fb9f0c43d6804ed13cbcdd413f892b50dd92
denysmarrow <mzmarrowz@gmail.com>
2022-01-30 23:37:32 -0500

    refactoring

--- src/com/careerdevs/RentalServiceTwo.java
+++ src/com/careerdevs/RentalServiceTwo.java
@@ -15,8 +15,9 @@ public class RentalServiceTwo {
         ArrayList<Car> rentedCars = new ArrayList<>();
 
         availableCars.add(car1);
-        availableCars.add(car3);
         availableCars.add(car2);
+        availableCars.add(car3);
+
 
 
         System.out.println("Available Cars");
@@ -25,29 +26,51 @@ public class RentalServiceTwo {
             System.out.println("(" + (i + 1) + ")" + availableCars.get(i).getName());
         }
 
+        while(true) {
+
         System.out.println("Enter a number to select the car you'd like to rent\n Selection:");
         int userSelection = scan.nextInt();
 
+        //
 
-        switch (userSelection) {
-            case 1:
-                System.out.println("Thank you! You are now renting the " + availableCars.get(0).getName());
-                break;
-            case 2:
-                System.out.println("Thank you! You are now renting the " + availableCars.get(1).getName());
-                break;
-            case 3:
-                System.out.println("Thank you! You are now renting the " + availableCars.get(2).getName());
-                break;
-            default:
-                System.err.println("Car selection invalid, please try again");
-                break;
+        if (userSelection > 0 && userSelection <= availableCars.size()) {
+            System.out.println("Thank you! You are now renting the " + availableCars.get(userSelection - 1).getName());
+
+
+
+        } else {
+            System.out.println("Invalid Option, please choose again");
+
+        }
+
+
+
+
+
+
 
 
         }
 
+
+
+
+
+
+
+
+
+        //        while (scan.hasNext()) {
+//            scan.nextLine();
+//
+//            for (int i = 0; i < availableCars.size(); i++)
+//                System.out.println("(" + (i + 1) + ")" + availableCars.get(i).getName());
+//
+//
+//
+//        }
+
+
     }
-
-
 }
 
