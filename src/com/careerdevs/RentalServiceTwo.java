c7ba9f423913e6a94dd842c6184c4b33d94667a6
denysmarrow <mzmarrowz@gmail.com>
2022-01-31 19:58:05 -0500

    refactoring

--- src/com/careerdevs/RentalServiceTwo.java
+++ src/com/careerdevs/RentalServiceTwo.java
@@ -7,6 +7,18 @@ public class RentalServiceTwo {
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        System.out.println("Welcome to the Car Rental CLI");
+        mainMenu();
+
+
+
+    }
+
+
+        private static void mainMenu (){
+
+
         Car car1 = new Car("Honda", "Accord");
         Car car2 = new Car("Chevy", "Cruze");
         Car car3 = new Car("Toyota", "Corolla");
@@ -19,38 +31,39 @@ public class RentalServiceTwo {
         availableCars.add(car3);
 
 
-
         System.out.println("Available Cars");
 
         for (int i = 0; i < availableCars.size(); i++) {
             System.out.println("(" + (i + 1) + ")" + availableCars.get(i).getName());
         }
 
-        while(true) {
+            while (true) {
 
-        System.out.println("Enter a number to select the car you'd like to rent\n Selection:");
-        int userSelection = scan.nextInt();
+            System.out.println("Enter a number to select the car you'd like to rent\n Selection:");
+            int userSelection = scan.nextInt();
 
-        //
+            //
 
-        if (userSelection > 0 && userSelection <= availableCars.size()) {
-            System.out.println("Thank you! You are now renting the " + availableCars.get(userSelection - 1).getName());
+            if (userSelection > 0 && userSelection <= availableCars.size()) {
+                System.out.println("Thank you! You are now renting the " + availableCars.get(userSelection - 1).getName());
 
 
+            } else {
+                System.out.println("Invalid Option, please choose again");
 
-        } else {
-            System.out.println("Invalid Option, please choose again");
+            }
+
+            System.out.println(userSelection);
+
+            if (userSelection > 0 && userSelection <= availableCars.size()) {
+
+                }
+
+            }
+
 
         }
-
-
-
-
-
-
-
-
-        }
+    }
 
 
 
@@ -71,6 +84,6 @@ public class RentalServiceTwo {
 //        }
 
 
-    }
-}
+
+
 
